c89ac4c61c03bbf37b8480ffb070aae3307be072
Warren Haskins <warrenhaskins1@gmail.com>
2022-02-07 18:57:22 -0500

    updated Thoughts Model

---
+++ models/Thoughts.js
@@ -0,0 +1,26 @@
+const { Schema, model, Schema} = require("mongoose");
+
+const thoughtsSchema = new Schema({
+thoughtText: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 280
+},
+createdAt: {
+type: Date,
+default: Date.now(),
+//Need to add the getter method for the timestamp
+},
+username: {
+    type: String,
+    required: true
+},
+// reactions: //NEED MORE INFO
+},
+//NEED VIRTUAL for reactionCount that retrieves the length of the toughts reactions array
+);
+
+const Thoughts = model("thoughts", thoughtsSchema);
+
+module.exports = Thoughts;
