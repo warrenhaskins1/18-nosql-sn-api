c7c6056101cacf541d126d2de3a8845a337edb9f
Warren Haskins <warrenhaskins1@gmail.com>
2022-02-08 20:04:11 -0500

    update to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,10 +24,63 @@ module.exports = {
 
   //Create a new User (Post)
   createUser(req, res) {
-      User.create(req.body)
+    User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
 
+  //findOneandUpdate
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
+  //findOneAndDelete
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id!" })
+          : User.findOneAndUpdate(
+              { users: req.params.userId },
+              { $pull: { users: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "User created but no user with this id!" })
+          : res.json({ message: "User successfully deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Remove User thoughts response
+  removeUserThoughts(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { thoughs: { thoughtsId: req.params.thoughtsId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
