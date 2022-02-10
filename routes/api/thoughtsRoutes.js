7f36255ab779e0ed4994a373e565126d39c05bfa
Warren Haskins <warrenhaskins1@gmail.com>
2022-02-10 13:24:22 -0500

    updated routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -3,8 +3,22 @@ const {
     getThoughts,
     getSingleThought,
     createThought,
+    updateThought,
     removeUserThoughts,
+    addReaction,
+    removeReaction,
 } = require("../../controllers/thoughtsController");
 
 // /api/thoughts
-router.route("/thoughts")
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/:thoughtId
+
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(removeUserThoughts);
+
+// Thought Reactions by :thoughtId
+
+router.route("/:thoughtId/reactions").post(addReaction);
+router.route("/:thoughtId/reactions").delete(removeReaction);
+
+module.exports = router;
